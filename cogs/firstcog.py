0a10c23506ea119390a272239ffd93bdeb846355
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-11 12:14:21 +0300

    Update firstcog.py

--- cogs/firstcog.py
+++ cogs/firstcog.py
@@ -11,6 +11,7 @@ from random import randint, choice, choices
 import io
 import sqlite3
 import random as r
+import typing 
 
 class user(commands.Cog):
 
@@ -20,7 +21,26 @@ class user(commands.Cog):
     @commands.command()
     async def example(self,ctx):
         await ctx.send("work")
+    
+    
 
+    
+    @commands.command(aliases = ['clear', 'c'])
+    @commands.has_permissions(manage_messages = True)
+    async def __clear(self, ctx, member: typing.Optional[discord.Member], amount : int):
+                await ctx.message.delete()
+
+                if member == None:
+                    await ctx.channel.purge(limit = amount)
+                elif member != None and member in ctx.guild.members:
+                    number = 0
+                    def predicate(message):
+                        return message.author == member
+                    async for elem in ctx.channel.history().filter(predicate):
+                        await elem.delete()
+                        number += 1
+                        if number >= amount:
+                            break
 
 
 def setup(client):
