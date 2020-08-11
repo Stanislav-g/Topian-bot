615b49e427d9c403055e7ca536b99c70fa62c90b
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-11 13:23:32 +0300

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -34,67 +34,38 @@ async def reload(ctx, extensions):
     await ctx.send('reloaded')
 
 @commands.Cog.listener()
-    async def clear_error( ctx, error ):
-        if isinstance( error, commands.MissingRequiredArgument ):
-            await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
+async def clear_error( ctx, error ):
+    if isinstance( error, commands.MissingRequiredArgument ):
+        await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
 
-        if isinstance( error, commands.MissingPermissions ):
-            await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
+    if isinstance( error, commands.MissingPermissions ):
+        await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
 
-    @ban.error    
-    async def ban_error( ctx, error ):
-        if isinstance( error, commands.MissingRequiredArgument ):
-            await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
+@commands.Cog.listener()   
+async def ban_error( ctx, error ):
+    if isinstance( error, commands.MissingRequiredArgument ):
+        await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
 
-        if isinstance( error, commands.MissingPermissions ):
-            await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
+    if isinstance( error, commands.MissingPermissions ):
+        await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
 
-    @commands.Cog.listener()    
-    async def unban_error( ctx, error ):
-        if isinstance( error, commands.MissingRequiredArgument ):
-            await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
+@commands.Cog.listener()  
+async def unban_error( ctx, error ):
+    if isinstance( error, commands.MissingRequiredArgument ):
+        await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
 
-        if isinstance( error, commands.MissingPermissions ):
-            await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
-    @commands.Cog.listener()  
-    async def kick_error( ctx, error ):
-        if isinstance( error, commands.MissingRequiredArgument ):
-            await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
+    if isinstance( error, commands.MissingPermissions ):
+        await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
+@commands.Cog.listener()   
+async def kick_error( ctx, error ):
+    if isinstance( error, commands.MissingRequiredArgument ):
+        await ctx.send( f'{ ctx.author.name }, обязательно укажите аргумент')
 
-        if isinstance( error, commands.MissingPermissions ):
-            await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
-   
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        ignored = commands.UserInputError
+    if isinstance( error, commands.MissingPermissions ):
+        await ctx.send( f'{ ctx.author.name }, у вас недостаточно прав ')
 
-        if isinstance(error, ignored):
-            return
-
-        elif isinstance(error, commands.CommandNotFound):
-            embed = discord.Embed(
-                description = 'Ошибка: данная команда не существует!\nДля получения списка всевозможных доступных команд, напишите <префикс>help.',
-                color = 0x800080
-            )
-            embed.set_footer(
-                text = f'{self.client.user.name} | все права закодированы.',
-                icon_url = self.bot.user.avatar_url
-            )
-            return await ctx.send(embed=embed)
-
-        elif isinstance(error, commands.MissingPermissions):
-            embed = discord.Embed(
-                description = 'Ошибка: у вас недостаточно прав для использования данной команды!',
-                color = 0x800080
-            )
-            embed.set_footer(
-                text = f'{self.client.user.name} | все права закодированы.',
-                icon_url = self.bot.user.avatar_url
-            )
-
-            return await ctx.send(embed=embed
-                                  
-                                  
+        
+        
 for filename in os.listdir('./cogs'): # Цикл перебирающий файлы в cogs
     client.load_extension(f'cogs.{filename[:-3]}') 
     
