64ddf3df8608100ae73a05e8efbe922191bd5782
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-12 12:52:10 +0300

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -103,17 +103,16 @@ class user(commands.Cog):
         
     #botinfo
     @commands.command()
-    async def botinfo(self, ctx ):
-        await ctx.channel.purge( limit = 1 )
-        emd = discord.Embed(title=f"{ctx.guild.name}", description="Информация о боте **NITAGAS bot**.\n  подробнее о командах  -help\n По вопросам обращатся на сервер https://discord.gg/NfTf9JD", color = 000000)
-        emd.add_field(name=f'**Меня создал:**', value="Stanislav", inline=True)  # Создает строку
-        emd.add_field(name=f'**Помощь в создании:**', value="", inline=True)  # Создает строку
-        emd.add_field(name=f'**Лицензия:**', value="Nitagas", inline=True)  # Создает строку
-        emd.add_field(name=f'**Я написан на:**', value="Discord.py", inline=True)  # Создает строку
-        emd.add_field(name=f'**Версия:**', value="1.0", inline=True)  # Создает строку
-        emd.add_field(name=f'**Патч:**', value="1.0", inline=True)  # Создает строку
-        emd.set_footer(text=f"© Copyright 2020 Stanislav | Все права защищены")  # создаение футера
-        await ctx.send( embed=emd )
+    async def botinfo( ctx )
+        emt = discord.Embed(title=f"{ctx.guild.name}", description="Информация о боте **NITAGAS bot**.\n  подробнее о командах  -help\n По вопросам обращатся на сервер https://discord.gg/NfTf9JD", color = 000000)
+        emt.add_field(name=f'**Меня создал:**', value="Stanislav", inline=True)  # Создает строку
+        emt.add_field(name=f'**Помощь в создании:**', value="yazdrim#6779", inline=True)  # Создает строку
+        emt.add_field(name=f'**Лицензия:**', value="Nitagas", inline=True)  # Создает строку
+        emt.add_field(name=f'**Я написан на:**', value="Discord.py", inline=True)  # Создает строку
+        emt.add_field(name=f'**Версия:**', value="1.0", inline=True)  # Создает строку
+        emt.add_field(name=f'**Патч:**', value="1.0", inline=True)  # Создает строку
+        emt.set_footer(text=f"© Copyright 2020 Stanislav | Все права защищены")  # создаение футера
+        await ctx.send(embed=emt)
     
 def setup(client):
     client.add_cog(user(client))
