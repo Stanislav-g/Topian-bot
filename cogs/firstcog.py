4f6e8a098479a29ec0ea3afa1241baeb16da5274
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-12 09:35:44 +0300

    Update firstcog.py

--- cogs/firstcog.py
+++ cogs/firstcog.py
@@ -46,7 +46,7 @@ class user(commands.Cog):
     async def __clear(self, ctx, member: typing.Optional[discord.Member], amount : int = None ):
                 await ctx.message.delete()
                
-                 if not Member:
+                 if Member == None:
                     await ctx.channel.purge(limit = amount)
                  if amount == None:
                     embw = discord.Embed( title = '**Info**', colour = discord.Color.green() )
