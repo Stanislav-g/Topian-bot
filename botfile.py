b6a816507a9351d6f1fa6c33786d86df09311aba
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-19 20:05:11 +0300

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -44,7 +44,26 @@ async def reload(ctx, extensions):
     client.unload_extension(f'cogs.{extensions}')# отгружаем ког
     client.load_extension(f'cogs.{extensions}')# загружаем 
     await ctx.send('reloaded')
-        
+    
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == 745689538608758806: # ID Сообщения
+        guild = client.get_guild(payload.guild_id)
+        role = None
+
+        if str(payload.emoji) == '1️⃣': # Emoji для реакций
+            role = guild.get_role(745685081489801246) # ID Ролей для выдачи
+        elif str(payload.emoji) == '2️⃣':
+            role = guild.get_role(745685081506709674)
+        elif str(payload.emoji) == '3️⃣':
+            role = guild.get_role(745685081548652594)
+        elif str(payload.emoji) == '4️⃣':
+            role = guild.get_role(745687846576455832)
+            
+        if role:
+            member = guild.get_member(payload.user_id)
+            if member:
+                await member.add_roles(role)          
         
 for filename in os.listdir('./cogs'): # Цикл перебирающий файлы в cogs
     client.load_extension(f'cogs.{filename[:-3]}') 
