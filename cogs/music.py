3783dfbda3ff474039271fa8083ca15bf116c474
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-22 22:08:25 +0300

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -27,7 +27,7 @@ class user(commands.Cog):
         k = (":2_:")
         await ctx.send(t)
     @commands.command()
-    async def message_edit(self, ctx, id:int = None, arg):
+    async def message_edit(self, ctx , * , arg = None, id:int = None):
         message = await ctx.message.channel.fetch_message(id)
         await message.edit(arg)
 
