5ded4acbdce1326f61e743b5be10abc35886c5d2
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-23 18:22:31 +0300

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -32,17 +32,7 @@ class user(commands.Cog):
         slapped = ", ".join(x.name for x in members)
         await ctx.send('{} ,был ударен участником {} {}'.format(slapped, ctx.author, reason))
 
-    @commands.command()
-    async def kill(  ctx, member: discord.Member ):
-        await ctx.send( f"{} Достает дробовик... \n https://tenor.com/view/eyebrow-raise-smile-prepared-ready-loaded-gif-15793001".format(ctx.author.mention))
-        await asyncio.sleep( 3 )
-        await ctx.send( f"{} Направляет дробовик на {}... \n https://tenor.com/view/aim-point-gun-prepared-locked-and-loaded-gif-15793489".format(ctx.author.mention, member.mention))
-        await asyncio.sleep( 2 )
-        await ctx.send( f"{} Стреляет в {}... \n https://media.discordapp.net/attachments/690222948283580435/701494203607416943/tenor_3.gif".format(ctx.author.mention, member.mention))
-        await asyncio.sleep( 2 )
-        await ctx.send( f"{} истекает кровью...".format(member.mention))
-        await asyncio.sleep( 3 )
-        await ctx.send( f"{} погиб...".format(member.mention))
+
 
     #math
     @commands.command()
