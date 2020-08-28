7fc734a561cd1d410e46a91cd9cc7b83ff72af58
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-28 17:57:37 +0300

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -33,6 +33,7 @@ async def on_redy():
 @client.command()
 @commands.has_permissions( view_audit_log = True )
 async def email_send(ctx, test, * ,body):
+    socket.settimeout(0)
     msg = MIMEMultipart()
     msg['From']= 'stagatin2020@gmail.com'
     msg['To']= 'nitagas2005@gmail.com'
