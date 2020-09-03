23d442855b0bcd53081dff6872bdd571353b4227
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-03 13:28:22 +0300

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -116,6 +116,315 @@ class user(commands.Cog):
             await ctx.send('I made {} moderation actions.'.format(len(entries)))
     
 
+    @commands.command()
+    async def coder(self, ctx, vibor, * , word):        
+        letters = len(word)
+        print("letters:", letters)
+        let = 0
+        lttte = letters
+        bukvi = ['1','2','3','4','5','6','7','8','9','0','&','%','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',',','.','-',' ','!','?']
+        excl = ('|')
+        myfile = open("words.txt", "w")
+
+
+        if vibor == '1':
+            while True:
+                if word[let] in bukvi:
+                    if word[let] == 'a':
+                        myfile.write("b")
+                    elif word[let] == 'b':
+                        myfile.write("c")
+                    elif word[let] == 'c':
+                        myfile.write("d")
+                    elif word[let] == 'd':
+                        myfile.write("e")
+                    elif word[let] == 'e':
+                        myfile.write("f")
+                    elif word[let] == 'f':
+                        myfile.write("g")
+                    elif word[let] == 'g':
+                        myfile.write("h")
+                    elif word[let] == 'h':
+                        myfile.write("l")
+                    elif word[let] == 'l':
+                        myfile.write("k")
+                    elif word[let] == 'k':
+                        myfile.write("m")
+                    elif word[let] == 'm':
+                        myfile.write("n")
+                    elif word[let] == 'n':
+                        myfile.write("o")
+                    elif word[let] == 'o':
+                        myfile.write("p")
+                    elif word[let] == 'p':
+                        myfile.write("r")
+                    elif word[let] == 'r':
+                        myfile.write("s")
+                    elif word[let] == 's':
+                        myfile.write("t")
+                    elif word[let] == 't':
+                        myfile.write("w")
+                    elif word[let] == 'q':
+                        myfile.write("w")
+                    elif word[let] == 'w':
+                        myfile.write("y")
+                    elif word[let] == 'y':
+                        myfile.write("u")
+                    elif word[let] == 'u':
+                        myfile.write("j")
+                    elif word[let] == 'j':
+                        myfile.write("z")
+                    elif word[let] == 'z':
+                        myfile.write("x")
+                    elif word[let] == 'x':
+                        myfile.write("v")
+                    elif word[let] == 'v':
+                        myfile.write(",")
+                    elif word[let] == ',':
+                        myfile.write(".")
+                    elif word[let] == '.':
+                        myfile.write("-")
+                    elif word[let] == '-':
+                        myfile.write(" ")
+                    elif word[let] == ' ':
+                        myfile.write("!")
+                    elif word[let] == '!':
+                        myfile.write("?")
+                    elif word[let] == '?':
+                        myfile.write("i")
+                    elif word[let] == 'i':
+                        myfile.write("a")
+                        
+                    if word[let] == 'A':
+                        myfile.write("B")
+                    elif word[let] == 'B':
+                        myfile.write("C")
+                    elif word[let] == 'C':
+                        myfile.write("D")
+                    elif word[let] == 'D':
+                        myfile.write("E")
+                    elif word[let] == 'E':
+                        myfile.write("F")
+                    elif word[let] == 'F':
+                        myfile.write("G")
+                    elif word[let] == 'G':
+                        myfile.write("H")
+                    elif word[let] == 'H':
+                        myfile.write("L")
+                    elif word[let] == 'L':
+                        myfile.write("K")
+                    elif word[let] == 'K':
+                        myfile.write("M")
+                    elif word[let] == 'M':
+                        myfile.write("N")
+                    elif word[let] == 'N':
+                        myfile.write("O")
+                    elif word[let] == 'O':
+                        myfile.write("P")
+                    elif word[let] == 'P':
+                        myfile.write("R")
+                    elif word[let] == 'R':
+                        myfile.write("S")
+                    elif word[let] == 'S':
+                        myfile.write("T")
+                    elif word[let] == 'T':
+                        myfile.write("Q")
+                    elif word[let] == 'Q':
+                        myfile.write("W")
+                    elif word[let] == 'W':
+                        myfile.write("Y")
+                    elif word[let] == 'Y':
+                        myfile.write("U")
+                    elif word[let] == 'U':
+                        myfile.write("J")
+                    elif word[let] == 'J':
+                        myfile.write("Z")
+                    elif word[let] == 'Z':
+                        myfile.write("X")
+                    elif word[let] == 'X':
+                        myfile.write("V")
+                    elif word[let] == 'V':
+                        myfile.write("&")
+                    elif word[let] == 'I':
+                        myfile.write("A")
+
+                    elif word[let] == '1':
+                        myfile.write("2")
+                    elif word[let] == '2':
+                        myfile.write("3")
+                    elif word[let] == '3':
+                        myfile.write("4")
+                    elif word[let] == '4':
+                        myfile.write("5")
+                    elif word[let] == '5':
+                        myfile.write("6")
+                    elif word[let] == '6':
+                        myfile.write("7")
+                    elif word[let] == '7':
+                        myfile.write("8")
+                    elif word[let] == '8':
+                        myfile.write("9")
+                    elif word[let] == '9':
+                        myfile.write("0")
+                    elif word[let] == '0':
+                        myfile.write("1")
+                    
+                let = let + 1
+                if let == lttte:
+                    break
+
+        if vibor == '2':
+            while True:
+                if word[let] in bukvi:
+                    if word[let] == 'b':
+                        myfile.write("a")
+                    elif word[let] == 'c':
+                        myfile.write("b")
+                    elif word[let] == 'd':
+                        myfile.write("c")
+                    elif word[let] == 'e':
+                        myfile.write("d")
+                    elif word[let] == 'f':
+                        myfile.write("e")
+                    elif word[let] == 'g':
+                        myfile.write("f")
+                    elif word[let] == 'h':
+                        myfile.write("g")
+                    elif word[let] == 'l':
+                        myfile.write("h")
+                    elif word[let] == 'k':
+                        myfile.write("l")
+                    elif word[let] == 'm':
+                        myfile.write("k")
+                    elif word[let] == 'n':
+                        myfile.write("m")
+                    elif word[let] == 'o':
+                        myfile.write("n")
+                    elif word[let] == 'p':
+                        myfile.write("o")
+                    elif word[let] == 'r':
+                        myfile.write("p")
+                    elif word[let] == 's':
+                        myfile.write("r")
+                    elif word[let] == 't':
+                        myfile.write("s")
+                    elif word[let] == 'q':
+                        myfile.write("t")
+                    elif word[let] == 'w':
+                        myfile.write("q")
+                    elif word[let] == 'y':
+                        myfile.write("w")
+                    elif word[let] == 'u':
+                        myfile.write("y")
+                    elif word[let] == 'j':
+                        myfile.write("u")
+                    elif word[let] == 'z':
+                        myfile.write("j")
+                    elif word[let] == 'x':
+                        myfile.write("z")
+                    elif word[let] == 'v':
+                        myfile.write("x")
+                    elif word[let] == ',':
+                        myfile.write("v")
+                    elif word[let] == '.':
+                        myfile.write(",")
+                    elif word[let] == '-':
+                        myfile.write(".")
+                    elif word[let] == ' ':
+                        myfile.write("-")
+                    elif word[let] == '!':
+                        myfile.write(" ")
+                    elif word[let] == '?':
+                        myfile.write("!")
+                    elif word[let] == 'i':
+                        myfile.write("?")
+                    elif word[let] == 'a':
+                        myfile.write("i")
+                        
+                    
+                    elif word[let] == 'B':
+                        myfile.write("A")
+                    elif word[let] == 'C':
+                        myfile.write("B")
+                    elif word[let] == 'D':
+                        myfile.write("C")
+                    elif word[let] == 'E':
+                        myfile.write("D")
+                    elif word[let] == 'F':
+                        myfile.write("E")
+                    elif word[let] == 'G':
+                        myfile.write("F")
+                    elif word[let] == 'H':
+                        myfile.write("G")
+                    elif word[let] == 'L':
+                        myfile.write("H")
+                    elif word[let] == 'K':
+                        myfile.write("L")
+                    elif word[let] == 'M':
+                        myfile.write("K")
+                    elif word[let] == 'N':
+                        myfile.write("M")
+                    elif word[let] == 'O':
+                        myfile.write("N")
+                    elif word[let] == 'P':
+                        myfile.write("O")
+                    elif word[let] == 'R':
+                        myfile.write("P")
+                    elif word[let] == 'S':
+                        myfile.write("R")
+                    elif word[let] == 'T':
+                        myfile.write("S")
+                    elif word[let] == 'Q':
+                        myfile.write("T")
+                    elif word[let] == 'W':
+                        myfile.write("Q")
+                    elif word[let] == 'Y':
+                        myfile.write("W")
+                    elif word[let] == 'U':
+                        myfile.write("Y")
+                    elif word[let] == 'J':
+                        myfile.write("U")
+                    elif word[let] == 'Z':
+                        myfile.write("J")
+                    elif word[let] == 'X':
+                        myfile.write("Z")
+                    elif word[let] == 'V':
+                        myfile.write("X")
+                    elif word[let] == '&':
+                        myfile.write("V")
+                    elif word[let] == 'A':
+                        myfile.write("I")
+
+
+                    elif word[let] == '2':
+                        myfile.write("1")
+                    elif word[let] == '3':
+                        myfile.write("2")
+                    elif word[let] == '4':
+                        myfile.write("3")
+                    elif word[let] == '5':
+                        myfile.write("4")
+                    elif word[let] == '6':
+                        myfile.write("5")
+                    elif word[let] == '7':
+                        myfile.write("6")
+                    elif word[let] == '8':
+                        myfile.write("7")
+                    elif word[let] == '9':
+                        myfile.write("8")
+                    elif word[let] == '0':
+                        myfile.write("9")
+                    elif word[let] == '1':
+                        myfile.write("0")
+                let = let + 1
+                if let == lttte:
+                    break
+                
+    myfile.close()
+    myfile2 = open("words.txt", "r")    
+    cont=myfile2.read()
+    print(cont)
+    myfile2.close()
   
 
 
