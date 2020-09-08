e5e70e6ecfb60c04ccbc02308ca035c82c34122b
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-08 14:28:03 +0200

    Update firstcog.py

--- cogs/firstcog.py
+++ cogs/firstcog.py
@@ -308,7 +308,7 @@ class user(commands.Cog):
             guild = ctx.guild
             ove = {ctx.author: discord.PermissionOverwrite(read_messages=True, send_messages=True), ctx.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True), ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False)}
 
-	    await guild.create_text_channel(name = {arg}, overwrites = ove)
+	    channel = await guild.create_text_channel(name = {arg})
 	    await ctx.send(embed = discord.Embed(description = f'**:keyboard: Текстовый канал "{arg}" успешно создан!**', color=0x0c0c0c))
 	
     #voice_create
