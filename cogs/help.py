f26da1da03c78bd43770fa4b69b4a5511028da68
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-10 11:34:20 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -20,8 +20,7 @@ class user(commands.Cog):
      #info
     @commands.command()
     async def bag(self, ctx, * ,arg):       
-        channel = client.get_channel( 705392639863685170 )
-        await ctx.send( embed = discord.Embed( title = 'Bag', description = f'Пользователь {ctx.author.name}, отправил баг\nБаг: {arg}', colour = discord.Color.red()) )
+        await ctx.send( embed = discord.Embed( description = f'Пользователь {ctx.author.name}, отправил баг\nБаг: {arg}') )
      
         
     #info\n
