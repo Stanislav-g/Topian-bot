d1f63afeeff67f97321feaebb8287dafa6235c15
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-10 11:21:06 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -22,12 +22,10 @@ class user(commands.Cog):
     async def bag(self, ctx, * ,arg):       
         channel = client.get_channel( 705392639863685170 )
         emb = discord.Embed( title = 'Bag', colour = discord.Color.red() )
-        emb.add_field( name = 'ИНФОРМАЦИЯ',value = 'Баг отправил {ctx.author.mention}\n')
-        
-        emb = discord.Embed(color=member.color if member.color != discord.Color.default() else discord.Color.red(), timestamp=datetime.datetime.now(datetime.timezone.utc), description=f'**{member.mention}, вы кикнуты**')
+        emb.add_field( name = 'ИНФОРМАЦИЯ',value = 'Баг отправил {ctx.author.mention}\nСообщение: {arg}')
         emb.set_author(name=ctx.author, icon_url=str(ctx.author.avatar_url_as(static_format='png', size=2048)))
-        emb.set_footer(timestamp=datetime.datetime.now(datetime.timezone.utc))
-        await member.send( embed = emb )        
+
+        await channel.send( embed = emb )        
         
     #info\n
     @commands.command()
