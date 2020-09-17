0f3e827924255fe751498199551a44f2392c6c61
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-17 16:28:07 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -101,7 +101,8 @@ class user(commands.Cog):
     @commands.command( pass_context = True )
     async def botinfo(self, ctx ):
         urll = 'https://discord.com/api/oauth2/authorize?client_id=742649758002315274&permissions=470412503&scope=bot'
-        emt = discord.Embed(title=f"{ctx.guild.name}", description="Информация о боте **Topian bot**.\n  подробнее о командах  =help\n По вопросам обращатся на сервер https://discord.gg/NfTf9JD\n Добавь меня на свой сервер https://discord.com/api/oauth2/authorize?client_id=742649758002315274&permissions=470412503&scope=bot", color = 0x00FF00, url = urll)
+        urlll = 'https://discord.com/api/oauth2/authorize?client_id=742649758002315274&permissions=470412503&scope=bot'            
+        emt = discord.Embed(title=f"{ctx.guild.name}", description="Информация о боте **Topian bot**.\n  подробнее о командах  =help\n По вопросам обращатся на сервер https://discord.gg/NfTf9JD\n Добавь меня на свой сервер https://discord.com/api/oauth2/authorize?client_id=742649758002315274&permissions=470412503&scope=bot", color = 0x00FF00, url = urll, url = urlll)
         emt.add_field(name=f'**Меня создал:**', value="Stanislav", inline=True)  # Создает строку
         emt.add_field(name=f'**Помощь в создании:**', value="Topian Team", inline=True)  # Создает строку
         emt.add_field(name=f'**Лицензия:**', value="TSBot", inline=True)  # Создает строку
