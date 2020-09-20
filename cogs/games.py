cdf2d6c5980a1df5556b489146b30528c2f33462
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-20 19:13:52 +0200

    Update games.py

--- cogs/games.py
+++ cogs/games.py
@@ -318,6 +318,8 @@ class user(commands.Cog):
 
     @commands.command()
     async def slapperson(self, ctx, members: commands.Greedy[discord.Member] = None, *, reason='no reason'):
+        if members == None:
+            await ctx.send('e')
         slapped = ", ".join(x.name for x in members)
         await ctx.send('{} ,был ударен участником {} {}'.format(slapped, ctx.author, reason))
         gif = random.choice(['https://tenor.com/view/back-slap-backhand-funny-animals-penguin-slap-gif-11724800','https://tenor.com/view/slap-bears-gif-10422113','https://tenor.com/view/gap-slapped-knockout-punch-gif-5122019','https://tenor.com/view/kevin-hart-slap-face-your-gif-10570690'])        
