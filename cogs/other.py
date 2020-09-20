c17fddbe998416697395d34054e56a2f020d4b2a
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-20 18:25:26 +0200

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -30,15 +30,10 @@ class user(commands.Cog):
        
     @commands.command()
     async def slapperson(self, ctx, member: discord.Member = None, *, reason='no reason'):
-	if member == None:
-            embw = discord.Embed( title = '**Info**', colour = discord.Color.green() )
-            embw.add_field( name = 'Kick',value = '**kick** = kick @user')
-            await ctx.send( embed = embw )
-        else:
-	    slapped = ", ".join(x.name for x in member)
-	    gif = random.choice(['https://tenor.com/view/back-slap-backhand-funny-animals-penguin-slap-gif-11724800','https://tenor.com/view/slap-bears-gif-10422113','https://tenor.com/view/gap-slapped-knockout-punch-gif-5122019','https://tenor.com/view/kevin-hart-slap-face-your-gif-10570690'])
-	    await ctx.send('{} ,был ударен участником {} {}'.format(slapped, ctx.author, reason))
-	    await ctx.send(gif)
+	slapped = ", ".join(x.name for x in member)
+	gif = random.choice(['https://tenor.com/view/back-slap-backhand-funny-animals-penguin-slap-gif-11724800','https://tenor.com/view/slap-bears-gif-10422113','https://tenor.com/view/gap-slapped-knockout-punch-gif-5122019','https://tenor.com/view/kevin-hart-slap-face-your-gif-10570690'])
+	await ctx.send('{} ,был ударен участником {} {}'.format(slapped, ctx.author, reason))
+	await ctx.send(gif)
         
         
     @commands.command()
