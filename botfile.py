aaa5d6e7237919d5a962ea033659b9a45a43d6f7
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-07 18:19:24 +0200

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -33,8 +33,22 @@ async def on_redy():
    
 @client.event
 async def on_guild_join(guild):
-    channel = client.get_channel( 747764481559494686 )
-    await channel.send(f"hi")
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            message = await channel.send(embed = discord.Embed(description = f"""Привет! Я Topian Bot, чтобы узнать мои команды напиши ``=help``"""))
+            invite = await channel.create_invite()
+
+            embed = discord.Embed(title=':white_check_mark: Guild hinzugefügt', type='rich', color=0x2ecc71) #Green
+            embed.set_thumbnail(url=guild.icon_url)
+            embed.add_field(name='Name', value=guild.name, inline=True)
+            embed.add_field(name='ID', value=guild.id, inline=True)
+            embed.add_field(name='Создатель сервера', value=f'{guild.owner} ({guild.owner.id})', inline=True)
+            embed.add_field(name='Регион', value=guild.region, inline=True)
+            embed.add_field(name='Людей на сервере', value=guild.member_count, inline=True)
+            embed.add_field(name='Сервер создан', value=guild.created_at, inline=True)
+            embed.add_field(name= 'Приглашение на сервер', value=invite, inline=True)
+            user = client.get_user(550061958938886175)
+            await user.send(embed=embed)
 	
 
 
