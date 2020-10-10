91d70529b185db6c67e85ee1262db1cc524edf26
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-10 13:16:22 +0200

    Update logs.py

--- cogs/logs.py
+++ cogs/logs.py
@@ -206,7 +206,26 @@ class user(commands.Cog):
                     embe.set_footer(text=f"Guild name: {guild.name}")
                     await member.send(embed=embe)
 
-                            
+    @commands.Cog.listener()
+    async def on_guild_join(guild):
+        client = commands.Bot( command_prefix = '=')
+        for channel in guild.text_channels:
+            if channel.permissions_for(guild.me).send_messages:
+                message = await channel.send(embed = discord.Embed(description = f"""Привет! Я Topian Bot, чтобы узнать мои команды напиши ``=help``"""))
+                invite = await channel.create_invite()
+
+                embed = discord.Embed(title=':white_check_mark: Добавили бота на новый сервер!', type='rich', color=0x2ecc71) #Green
+                embed.set_thumbnail(url=guild.icon_url)
+                embed.add_field(name='Name', value=guild.name, inline=True)
+                embed.add_field(name='ID', value=guild.id, inline=True)
+                embed.add_field(name='Создатель сервера', value=f'{guild.owner})', inline=True)
+                embed.add_field(name='Регион', value=guild.region, inline=True)
+                embed.add_field(name='Людей на сервере', value=guild.member_count, inline=True)
+                embed.add_field(name='Сервер создан', value=guild.created_at, inline=True)
+                embed.add_field(name= 'Приглашение на сервер', value=invite, inline=True)
+                userd = self.client.get_user(550061958938886175)
+                await userd.send(embed=embed)
+	                            
     @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel):
         client = commands.Bot( command_prefix = '=')
