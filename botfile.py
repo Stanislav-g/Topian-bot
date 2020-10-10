7948ebb92c264deafc7a5889cdb491cdad99dd61
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-10 13:08:03 +0200

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -43,7 +43,7 @@ async def on_guild_join(guild):
             embed.set_thumbnail(url=guild.icon_url)
             embed.add_field(name='Name', value=guild.name, inline=True)
             embed.add_field(name='ID', value=guild.id, inline=True)
-            embed.add_field(name='Создатель сервера', value=f'{guild.owner} ({guild.owner.id})', inline=True)
+            embed.add_field(name='Создатель сервера', value=f'{guild.owner})', inline=True)
             embed.add_field(name='Регион', value=guild.region, inline=True)
             embed.add_field(name='Людей на сервере', value=guild.member_count, inline=True)
             embed.add_field(name='Сервер создан', value=guild.created_at, inline=True)
