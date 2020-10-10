324c56801f692c7b50f6ab9126d5dec21d212857
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-10 16:13:58 +0200

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -40,7 +40,7 @@ async def on_guild_join(guild):
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
             message = await channel.send(embed = discord.Embed(description = f"""Привет! Я Topian Bot, чтобы узнать мои команды напиши ``=help``"""))
-	    user = client.get_user('550061958938886175')
+	    user = client.get_user(550061958938886175)
             await user.send(f"embed=embed")
             break
 
