6bd981d9a679e7b7a2351112e15c822ba87cdf86
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-10 13:52:00 +0200

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -36,11 +36,13 @@ async def on_redy():
 
 @client.event
 async def on_guild_join(guild):
+    client = commands.Bot( command_prefix = '=')
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
             message = await channel.send(embed = discord.Embed(description = f"""Привет! Я Topian Bot, чтобы узнать мои команды напиши ``=help``"""))
             invite = await channel.create_invite()
-
+            user = client.get_user(550061958938886175)
+	    await channel.send(user)
             embed = discord.Embed(title=':white_check_mark: Бота добавили на новый сервер!', type='rich', color=0x2ecc71) #Green
             embed.set_thumbnail(url=guild.icon_url)
             embed.add_field(name='Name', value=guild.name, inline=True)
@@ -50,8 +52,6 @@ async def on_guild_join(guild):
             embed.add_field(name='Людей на сервере', value=guild.member_count, inline=True)
             embed.add_field(name='Сервер создан', value=guild.created_at, inline=True)
             embed.add_field(name= 'Приглашение на сервер', value=invite, inline=True)
-            user = client.get_user(550061958938886175)
-	    await channel.send(user)
             await user.send(embed=embed)
             break
 
