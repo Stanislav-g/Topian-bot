bae141aed98465b9c89b2cb78053ba9f6dd3a3e4
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-11 12:48:52 +0200

    Update reactionadd.py

--- cogs/reactionadd.py
+++ cogs/reactionadd.py
@@ -28,9 +28,10 @@ class user(commands.Cog):
     collectionshop = db["shop"]
     collectionticket = db["ticket"]
     collectionlogschannels = db["logschannels"]
-
+    collectionreaction = db["reaction"]
 
    
+
     @commands.has_permissions(administrator = True)     
     @commands.command()
     async def module_reaction(self, ctx, arg = None):
@@ -43,7 +44,7 @@ class user(commands.Cog):
         collectionticket = db["ticket"]
         collectionlogschannels = db["logschannels"]
         if not arg:
-            await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** вы не написали что хотите сделать, включить или выключить модуль!``=module_lvls on`` ``=module_lvls off`` """))
+            await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** вы не написали что хотите сделать, включить или выключить модуль!``=module_reaction on`` ``=module_reaction off`` """))
         elif arg == 'on':
             name = 'economy'
             num = ctx.author.guild.id
@@ -56,7 +57,7 @@ class user(commands.Cog):
                 allnum = str(num) + str(num2)
                 on = 'on'
                 collectionmodules.insert_one({"_id": allnum, "name": name, "on_off": 0, "lvls": on, "rep": 0, "ticket": 1, "warns": 0, "logs": 0, "reaction": 0})
-                await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней успешно включен!"""))
+                await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций успешно включен!"""))
             
             else:
                 num = ctx.author.guild.id
@@ -64,14 +65,14 @@ class user(commands.Cog):
                 allnum = str(num) + str(num2)
                 stat = collectionmodules.find_one({"_id": allnum})["reaction"]
                 if stat == 'on':
-                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней уже включен!"""))
+                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций уже включен!"""))
                 else:
                     num = ctx.author.guild.id
                     num2 = '111'
                     allnum = str(num) + str(num2)
                     on = 'on'
                     guild = collectionmodules.update_one({"_id": allnum}, {"$set": {"reaction": on}})
-                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней успешно включен!"""))
+                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций успешно включен!"""))
 
         elif arg == 'off':
             name = 'economy'
@@ -85,18 +86,18 @@ class user(commands.Cog):
                 allnum = str(num) + str(num2)
                 off = 'off'
                 collectionmodules.insert_one({"_id": allnum, "name": name, "on_off": 0, "lvls": off, "rep": 0, "ticket": 1, "warns": 0, "logs": 0, "reaction": 0})
-                await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней успешно выключен!"""))
+                await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций успешно выключен!"""))
             else:
                 stat = collectionmodules.find_one({"_id": allnum})["reaction"]
                 if stat == 'off':
-                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней уже выключен!"""))
+                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций уже выключен!"""))
                 else:
                     num = ctx.author.guild.id
                     num2 = '111'
                     allnum = str(num) + str(num2)
                     off = 'off'
                     guild = collectionmodules.update_one({"_id": allnum}, {"$set": {"reaction": off}})
-                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль уровней выключен!"""))
+                    await ctx.send(embed = discord.Embed(description = f"""**{ctx.author}** модуль реакций выключен!"""))
         else:
             await ctx.send(f"На данном сервере не создана база данных, ее можно создать командой")
 
@@ -118,17 +119,21 @@ class user(commands.Cog):
         num2 = '111'
         allnum = str(num) + str(num2)
         if collectionmodules.count_documents({"_id": allnum}) == 1:
+            await ctx.send("1")
             num = ctx.author.guild.id
             num2 = '111'
             allnum = str(num) + str(num2)
             stat = collectionmodules.find_one({"_id": allnum})["reaction"]
             if stat == 'on':
+                await ctx.send("2")
                 for channel in ctx.guild.text_channels:
                     ch = int(channel.id)
                     argg = int(arg)
                     if argg == ch:
+                        await ctx.send("3")
                         guild = ctx.guild.id
                         if collectionreaction.count_documents({"_id": guild}) == 0:
+                            await ctx.send("4")
                             collectionlogschannels.insert_one({"_id": guild, "reactionchannel": argg})
                             await ctx.send("Канал авто-реакций установлен!")
                             break
@@ -156,23 +161,24 @@ class user(commands.Cog):
         collectionmodules = db["modules"]
         collectionshop = db["shop"]
         collectionticket = db["ticket"]
-        collectionlogschannels = db["logschannels"]
+        collectionreaction = db["reaction"]
         num = message.guild.id
         num2 = '111'
         allnum4 = str(num) + str(num2)
         if collectionmodules.count_documents({"_id": allnum4}) == 1:
             if collectionmodules.find_one({"_id": allnum4})["reaction"] == 'on':
                 num = message.author.guild.id
-                if collectionlogschannels.count_documents({"_id": num}) == 0:
+                if collectionreaction.count_documents({"_id": num}) == 0:
                     pass
                     
                 else:
-                    stat = collectionlogschannels.find_one({"_id": num})["reaction"]
+                    stat = collectionreaction.find_one({"_id": num})["reaction"]
                     if stat != '0':
                         await message.add_reaction('✅')
                         await message.add_reaction('❌') 
 
                         
+
 
 
 def setup(client):
