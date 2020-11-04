6e58236e5556504f327865185fd0d22557dfbdca
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-11-04 09:27:55 +0100

    Update botfile.py

--- botfile.py
+++ botfile.py
@@ -199,8 +199,8 @@ async def on_command_error(ctx, err):
         guildf = ctx.guild.name
         userf = ctx.author.name
         channel = client.get_channel( 773461826696380426 )
-	errorall = str({err}) + str("\n") + str({guildf})
-        await channel.send("{err}\n {guildf}")
+	errorall = str({err}) + str("\n")
+        await channel.send({errorall})
 
 
 
