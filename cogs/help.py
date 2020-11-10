05f18f54df846cb1af313f17e7b5760892e1769e
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-11-10 12:57:11 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -68,7 +68,7 @@ class user(commands.Cog):
         embeds = [embed1, embed2, embed3, embed4, embed5, embed6, embed7]
         message = await ctx.send(embed = embed1)
         react = ['❌']
-        page = Paginator(client, message, only=ctx.author, use_more=False, embeds=embeds, timeout=60, use_exit=True,exit_reaction=react, color=#7FFF00)
+        page = Paginator(self.client, message, only=ctx.author, use_more=False, embeds=embeds, timeout=60, use_exit=True,exit_reaction=react, color=#7FFF00)
         await page.start()
 
 
